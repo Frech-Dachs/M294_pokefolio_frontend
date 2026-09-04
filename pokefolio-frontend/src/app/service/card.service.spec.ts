0d6718e8e3234a3dba46870c9522db5a8931736d
Leon <leontroller@icloud.com>
2026-09-04 10:41:44 +0200

    tests

---
+++ pokefolio-frontend/src/app/service/card.service.spec.ts
@@ -0,0 +1,90 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+
+import { CardService } from './card.service';
+import { CardType } from '../data/card-type';
+import { Card } from '../data/card';
+import { environment } from '../../environments/environment';
+
+describe('CardService', () => {
+  let service: CardService;
+  let httpMock: HttpTestingController;
+
+  const baseUrl = `${environment.backendBaseUrl}card`;
+
+  const fakeCard: Card = {
+    id: 1,
+    cardType: CardType.POKEMON,
+    name: 'Pikachu',
+    type: 'Electric',
+    hp: 60,
+    rarity: 'Common',
+    setName: 'Base Set',
+    cardNumber: '25/102',
+    imageUrl: 'https://example.com/pikachu.png'
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+      teardown: { destroyAfterEach: true }
+    });
+    service = TestBed.inject(CardService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get all cards', () => {
+    service.getAll().subscribe(cards => expect(cards).toHaveLength(1));
+
+    const req = httpMock.expectOne(baseUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush([fakeCard]);
+  });
+
+  it('should get a single card by id', () => {
+    service.getById(1).subscribe(card => expect(card).toEqual(fakeCard));
+
+    const req = httpMock.expectOne(`${baseUrl}/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(fakeCard);
+  });
+
+  it('should create a card', () => {
+    const { id, ...newCard } = fakeCard;
+
+    service.create(newCard).subscribe(card => expect(card).toEqual(fakeCard));
+
+    const req = httpMock.expectOne(baseUrl);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newCard);
+    req.flush(fakeCard);
+  });
+
+  it('should update a card', () => {
+    const { id, ...updatedCard } = fakeCard;
+
+    service.update(1, updatedCard).subscribe(card => expect(card).toEqual(fakeCard));
+
+    const req = httpMock.expectOne(`${baseUrl}/1`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(updatedCard);
+    req.flush(fakeCard);
+  });
+
+  it('should delete a card', () => {
+    service.delete(1).subscribe();
+
+    const req = httpMock.expectOne(`${baseUrl}/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
+  });
+});
